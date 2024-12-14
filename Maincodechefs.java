fa33632f7ad5d55925bee77176cceeacddf1d9f4
JaswithaG <gummijaswitha@gmail.com>
2024-12-14 13:50:22 +0530

    Add files via upload

---
+++ Maincodechefs.java
@@ -0,0 +1,5 @@
+public class Maincodechefs {
+    public static void main(String[] args) {
+        System.out.println("Code" + "Chef");
+    }
+}
---
+++ arithmetic.java
@@ -0,0 +1,5 @@
+public class arithmetic{
+    public static void main(String[] args) {
+        System.out.println(7.0 / 2);
+    }
+}
