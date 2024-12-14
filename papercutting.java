0d12cbf04e81e3108ca633478d8625e31e39f155
JaswithaG <gummijaswitha@gmail.com>
2024-12-14 12:28:05 +0530

    Add files via upload

---
+++ papercutting.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int t=0;t<T;t++){
+		    int N=sc.nextInt();
+		    int K=sc.nextInt();
+		    int markSquares=(N/k)*(N/K);
+		    System.out.println(maxSquares);
+		}
+		sc.close()
+
+	}
+}
